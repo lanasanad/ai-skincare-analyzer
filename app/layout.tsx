6f1767fc38dc7da7edaa6f0a90fa4cf6c4634bd0
Lana Sanad <lana.sanad@gmail.com>
2024-08-13 19:51:58 +1200

    change document title

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Analyze Your Skincare with Glow AI",
+  title: "Glow AI",
 };
 
 export default function RootLayout({
